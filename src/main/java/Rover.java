49340b9f36cf689044708d9789e236042306429c
10204961dw <605920599@qq.com>
2019-12-28 16:53:26 +0800

    refactor

--- src/main/java/Rover.java
+++ src/main/java/Rover.java
@@ -9,11 +9,11 @@ public class Rover {
 	private int curY = 0;
 	private String direction;
 	public void land(Area area, int x, int y, String direction) throws Exception {
-		if(!area.containsPointWithX(x)) {
-			throw new Exception("curX=" +x+ " is out of area");
+		if(!area.containsPointWithX(x)){
+			throw new Exception("curX="+x+" is out of area");
 		}
 		if(!area.containsPointWithY(y)) {
-			throw new Exception("Y=" +y+ " is out of area");
+			throw new Exception("Y="+y+" is out of area");
 		}
 		this.area = area;
 		this.curX = x;
