23e3a70b74a09881713105499ddb8893f76c4027
10204961dw <605920599@qq.com>
2019-12-28 16:27:23 +0800

    refactor

--- src/main/java/RoverController.java
+++ src/main/java/RoverController.java
@@ -11,7 +11,6 @@ public class RoverController {
             return "";
         }
         Area area = new Area(Integer.parseInt(info[0]), Integer.parseInt(info[1]));
-        Rover rover = new Rover();
         rover.land(area, Integer.parseInt(info[2]), Integer.parseInt(info[3]), info[4]);
 
         for (int i = 4; i < info.length; i++) {
